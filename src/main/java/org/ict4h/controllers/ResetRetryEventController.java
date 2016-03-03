5bbc68aa8aa3e8024f27f623d3d924aa29247f44
anukurian16 <akurian@INakurian.local>
2016-03-03 14:52:19 +0530

    removed unused headers

--- src/main/java/org/ict4h/controllers/ResetRetryEventController.java
+++ src/main/java/org/ict4h/controllers/ResetRetryEventController.java
@@ -1,19 +1,14 @@
 package org.ict4h.controllers;
 
-import com.fasterxml.jackson.annotation.JacksonInject;
-import org.ict4h.atomfeed.client.domain.FailedEvent;
 import org.ict4h.domain.AppConfig;
 import org.ict4h.service.AppConfiguration;
 import org.ict4h.service.FailedEventService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 import java.sql.SQLException;
-import java.util.List;
 
 @Controller
 public class ResetRetryEventController {
