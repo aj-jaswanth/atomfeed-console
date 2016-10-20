00acd85902c075350fe191ba78ffd580b2f67bef
anukurian16 <akurian@in-akurian.local>
2016-10-20 14:22:51 +0530

    modified pom to include preinstall and postinstall scripts

---
+++ src/scripts/atomfeedConsole/postinstall.sh
@@ -0,0 +1,2 @@
+sudo ln -sf /etc/atomfeedConsole/atomfeedConsole /etc/rc.d/rc.local
+chmod +x /etc/atomfeedConsole/atomfeedConsole
---
+++ src/scripts/atomfeedConsole/preinstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "pre install"
+rm -rf /var/log/atomfeedConsole/atomfeedConsole.log
+rm -rf /opt/atomfeed-console-1.0-SNAPSHOT.jar
+rm -rf /etc/atomfeedConsole/application.yml
