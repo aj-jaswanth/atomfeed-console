a03131486319d88e9b2b6592cbdb4fd0620d3d74
anukurian16 <akurian@in-akurian.local>
2016-10-24 12:39:46 +0530

    script added as service

--- src/scripts/atomfeedConsole/postinstall.sh
+++ src/scripts/atomfeedConsole/postinstall.sh
@@ -1,2 +1,8 @@
-sudo ln -sf /etc/atomfeedConsole/atomfeedConsole /etc/rc.d/rc.local
-chmod +x /etc/atomfeedConsole/atomfeedConsole
+groupadd atomfeed-console
+useradd -g atomfeed-console atomfeed-console
+mkdir /opt/atomfeedConsole/var
+mkdir /opt/atomfeedConsole/var/log
+chmod +x /opt/atomfeedConsole/etc/atomfeed-console
+chown -R atomfeed-console:atomfeed-console /opt/atomfeedConsole
+sudo ln -s /opt/atomfeedConsole/etc/atomfeed-console /etc/init.d/atomfeed-console
+sudo chkconfig --add atomfeed-console
--- src/scripts/atomfeedConsole/preinstall.sh
+++ src/scripts/atomfeedConsole/preinstall.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 echo "pre install"
-rm -rf /var/log/atomfeedConsole/atomfeedConsole.log
-rm -rf /opt/atomfeed-console-1.0-SNAPSHOT.jar
-rm -rf /etc/atomfeedConsole/application.yml
+rm -rf /opt/atomfeedConsole
