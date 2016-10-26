2163cc9537d0f7dc07d4c21f7a5dc7b7094affcb
anukurian16 <akurian@in-akurian.local>
2016-10-26 14:12:15 +0530

    required symlinks added, no login user added

--- src/scripts/atomfeedConsole/postinstall.sh
+++ src/scripts/atomfeedConsole/postinstall.sh
@@ -1,8 +1,13 @@
 groupadd atomfeed-console
-useradd -g atomfeed-console atomfeed-console
-mkdir /opt/atomfeedConsole/var
-mkdir /opt/atomfeedConsole/var/log
-chmod +x /opt/atomfeedConsole/etc/atomfeed-console
-chown -R atomfeed-console:atomfeed-console /opt/atomfeedConsole
-sudo ln -s /opt/atomfeedConsole/etc/atomfeed-console /etc/init.d/atomfeed-console
+sudo usermod -s /usr/sbin/nologin atomfeed-console
+mkdir /opt/atomfeed-console/var
+mkdir /opt/atomfeed-console/var/log
+
+chmod +x /opt/atomfeed-console/etc/atomfeed-console
+chown -R atomfeed-console:atomfeed-console /opt/atomfeed-console
+
+sudo ln -s /opt/atomfeed-console/etc/application.yml /etc/atomfeed-console.yml
+sudo ln -s /opt/atomfeed-console/etc/log4j.properties /etc/log4j.properties
+sudo ln -s /opt/atomfeed-console/var/log/atomfeed-console.log /var/log/atomfeed-console.log
+sudo ln -s /opt/atomfeed-console/etc/atomfeed-console /etc/init.d/atomfeed-console
 sudo chkconfig --add atomfeed-console
--- src/scripts/atomfeedConsole/preinstall.sh
+++ src/scripts/atomfeedConsole/preinstall.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 echo "pre install"
-rm -rf /opt/atomfeedConsole
+rm -rf /opt/atomfeed-console
+sudo rm -rf /etc/init.d/atomfeed-console
+sudo rm -rf /etc/atomfeed-console.yml
+sudo rm -rf /etc/log4j.properties
+sudo rm -rf /var/log/atomfeed-console.log
